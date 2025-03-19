adb6328be66b2aeba6154e992151094745fbec4b
hafzanasim <hafzanasim@gmail.com>
2025-03-19 02:43:50 +0000

    updated main

--- main.py
+++ main.py
@@ -13,22 +13,43 @@ tasks = [
 def root():
     return "Hello World"
 
-@app.get("/get-tasks")
+@app.get("/tasks/all")
 def get_all_tasks():
     return tasks
 
-# @app.get("/")
-# def get_task():
-#     return "Hello World"
+@app.get("/get-task/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task.id == task_id:
+            return task
 
-# @app.put("/")
-# def update_task():
-#     return "Hello World"
+    return "Task Not Found"
 
-# @app.post("/")
-# def create_task():
-#     return "Hello World"
+@app.get("/")
+def root():
+     return "Hello World"
 
-# @app.delete("/")
-# def delete_task():
-#     return "Hello World"
+@app.put("/{task_id}")
+def update_task(task_id: int, updated: Task):
+    for task in tasks:
+        if task.id == task_id:
+            task.description = updated.description
+            task.isComplete = updated.isComplete
+            return "Task Updated"
+
+    return "Task Not Found"
+
+
+@app.post("/tasks")
+def create_task(task_model: Task):
+    tasks.append(task_model)
+    return "Task Added"
+
+@app.delete("/task/delete{task_id}")
+def delete_task(task_id: int):
+    for task in tasks:
+        if task.id == task_id:
+            tasks.remove(task)
+            return "Task Deleted"
+
+    return "Task Not Found"
